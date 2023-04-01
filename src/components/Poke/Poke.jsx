08f4f4a2177b17499186b4ec02c2daf0afdfec57
Ezequiel Garcia <ezequieladriangarcia@gmail.com>
2023-04-01 14:05:27 -0300

    clase 3 useEffect

--- src/components/Poke/Poke.jsx
+++ src/components/Poke/Poke.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Poke.css"
 
 export const Poke = () => {
+    const [pokemon, setPokemon] = useState ("");
+    const [id, setId] = useState (1);
+    const [busqueda,setBusqueda] = useState ("");
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then((res) => res.json())
+            .then((data) =>{
+                setPokemon({
+                    numero: data.id,
+                    nombre: data.name,
+                    imagen: data.sprites.front_default,
+                })
+            })
+    }, [])
     return (
         <>
-           <h4 className="poke">Poke</h4>
+            <h4 className="poke">{pokemon.numero}</h4>
+            <h5 className="poke">{pokemon.nombre}</h5>
+            <img src={pokemon.imagen}/>
         </>
     )
 }
